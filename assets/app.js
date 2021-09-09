b26cf523e8fca332ce7e57537646930afe86b34b
Ouassim-Hamdani <83837581+Ouassim-Hamdani@users.noreply.github.com>
2021-09-10 01:17:33 +0200

    Add files via upload

---
+++ assets/app.js
@@ -0,0 +1,61 @@
+var lastScrollTop = 0;
+    navbar = document.getElementById("navbar");
+window.addEventListener("scroll", function(){
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > lastScrollTop && document.getElementById("burger").className === "burger") {
+        navbar.style.top = "-80px";
+
+    } else {
+        navbar.style.top="0";
+    }
+    lastScrollTop = scrollTop;
+})
+function quit() {
+    document.getElementById("burger").click()
+}
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    });
+}
+
+
+navSlide();
+
+$(".link").on("click", function(e) {
+    console.log(this.hash);
+    if (this.hash !== '') {
+        e.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        },800);
+    }
+});
+
+const sections = document.querySelectorAll("section");
+const NavList = document.querySelectorAll("nav ul li");
+window.addEventListener("scroll", ()=>{
+    let current = '';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if (pageYOffset > sectionTop - sectionHeight /2) {
+            current = section.getAttribute('id');
+
+        }
+    })
+    NavList.forEach(li => {
+        li.classList.remove("active");
+        if (li.classList.contains(current)) {
+            li.classList.add('active');
+            
+        }
+    })
+})
